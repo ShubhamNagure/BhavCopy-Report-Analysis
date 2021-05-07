8c1cc1aa7a265103d98caaf02be5ae0b77a40b73
ShubhamNagure <nagure.shubham@gmail.com>
2021-05-07 23:03:07 +0530

    getData module is created and input file is ready

--- Project/util/getData.py
+++ Project/util/getData.py
@@ -1,23 +1,16 @@
 from urllib.request import Request, urlopen
-import shutil
+import shutil ,os
 import datetime ,time 
 from datetime import date,timedelta
+
+from zipfile import ZipFile
+import pandas as pd
 
 
 
 weekdayNum=datetime.datetime.now().weekday()
 current_time = datetime.datetime.now()
 right_now=time.strftime("%H%M")
-# if weekdayNum < 5:
-#     if right_now == "1800":
-#         print("1800")
-#     else:
-#         sysdate=current_time.strftime("%d%m")
-#         sysday= int(current_time.strftime("%d"))
-#         print(sysday)
-# elif weekdayNum == 5 :
-#     sysday= current_time.strftime("%d")-1
-
 
 
 if weekdayNum<5:
@@ -26,6 +19,14 @@ if weekdayNum<5:
             filedate = (date.today() - timedelta(days = 3)).strftime("%d%m%y")
         else:
             filedate = (date.today() - timedelta(days = 1)).strftime("%d%m%y")
+    else:
+        print("AFTER 6 PM ")
+        filedate = date.today().strftime("%d%m%y")
+        print(filedate)
+else:
+    print("WEEKENDS")
+    filedate = (date.today() - timedelta(days = weekdayNum-4)).strftime("%d%m%y")
+
 
 
 link = f"https://www.bseindia.com/download/BhavCopy/Equity/EQ{filedate}_CSV.ZIP"
@@ -46,12 +47,48 @@ def download_file(link, file_name, length):
         with open(file_name, 'wb') as writer:
             request = urlopen(req, timeout=3)
             shutil.copyfileobj(request, writer, length)
+            print('File downloaded with success!')
     except Exception as e:
         print('File cannot be downloaded:', e)
-    finally:
-        print('File downloaded with success!')
 
+        
 file_name = 'EQ'+filedate+'_CSV.ZIP'
 length = 2048
 download_file(link, file_name, length)
+
+
+"""create directory and move zip file there and unzip there."""
+
+def move_and_unzip(file_name):
+    #create folder with file date
+    directory = f"{filedate}"
+    parent_dir = "F:/BhavCopy-Report-Analysis/BhavCopy-Report-Analysis/Project/util/data/"
+
+    path = os.path.join(parent_dir, directory)
+    os.mkdir(path)
+
+    #move file to newly created dir
+    source_dir = 'F:/BhavCopy-Report-Analysis/BhavCopy-Report-Analysis/Project/util/'
+    target_dir = f'F:/BhavCopy-Report-Analysis/BhavCopy-Report-Analysis/Project/util/data/{filedate}'
+    shutil.move(os.path.join(source_dir, file_name), target_dir)
+
+    #change dir to unzip that file
+    os.chdir(target_dir)
+    with ZipFile(file_name, 'r') as zip:
+
+        zip.printdir()
+        print('Extracting all the files now...')
+        zip.extractall()
+        print('Done!')
+
+    #header are removed and created input file.
+    csv_file_name ='EQ'+filedate+'.csv'
+    df=pd.read_csv(csv_file_name)
+    df.to_csv('input.csv', index_label=None, header=False)
+
+move_and_unzip(file_name)
+
+
+
+
 
