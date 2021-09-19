fffbea496f41f99e3dfb3fc488e2e32b15343c19
ShubhamNagure <nagure.shubham@gmail.com>
2021-09-19 17:39:52 +0000

    db setting reverted

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME':'/home/shubham/BhavCopy-Report-Analysis/Project/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
