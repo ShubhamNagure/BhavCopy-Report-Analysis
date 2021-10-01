e564516c5a9f6e469e3b807dd57f5b7c74d6c4a0
ShubhamNagure <nagure.shubham@gmail.com>
2021-10-01 17:58:21 +0000

    after login page is loading all the data

--- Project/MainApp/views.py
+++ Project/MainApp/views.py
@@ -17,6 +17,62 @@ r = redis.Redis(
 host='127.0.0.1',
 port='6379')
 
+def rendAllStocks():
+    print("All data load on startup")
+    myallkeys = []
+    for k in r.keys():
+        myallkeys.append(k)
+
+    myallvalues =[]
+    for s in myallkeys:
+        myallvalues.append(r.hgetall(s))
+
+    finalAllList =[]
+    for n in myallvalues:
+        finalAllList.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in n.items()})
+    newlist = sorted(finalAllList, key=lambda k: k['SC_CODE']) 
+    jsonObj= json.dumps(newlist,separators=(',', ':')).replace("'", r"\'")
+    return jsonObj
+    
+def searchStocks(request):
+    filter_rec = request.GET.get('stock')
+ 
+    uperc=filter_rec.upper()
+    if cache.get(uperc):
+        # uperc=filter_rec.upper()
+        jsonObj = cache.get(uperc)
+        # print(newlist)
+        print("DATA COMING FROM CACHE")
+    else:
+        # uperc=filter_rec.upper()
+        req_str = f'bhavcopy:{uperc}*'
+        mylist= []
+        for key in r.scan_iter(req_str):
+            mylist.append(key.decode('utf-8'))
+        mylistdeteils = []
+        for l in mylist:
+            mylistdeteils.append(r.hgetall(l))
+        finallist=[]
+        for x in mylistdeteils:
+            finallist.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in x.items()})
+
+        newlist = sorted(finallist, key=lambda k: k['SC_CODE']) 
+        jsonObj= json.dumps(newlist,separators=(',', ':')).replace("'", r"\'")
+        cache.set(uperc, jsonObj,timeout=500)
+        print("DATA COMING FROM DB")
+    return jsonObj
+
+@login_required(login_url='login')
+def redisDBandCache(request):
+    filter_rec = request.GET.get('stock')
+
+    if filter_rec is not None:
+        jsonObj=searchStocks(request)
+    else:
+        jsonObj=rendAllStocks()
+    context = {'jsonObj': jsonObj}
+    return render(request, 'index.html', context )
+
 
 def registerPage(request):
     if request.user.is_authenticated:
@@ -36,7 +92,14 @@ def registerPage(request):
 
 def loginPage(request):
     if request.user.is_authenticated:
-        return render(request,'index.html')
+        filter_rec = request.GET.get('stock')
+
+        if filter_rec is not None:
+            jsonObj=searchStocks(request)
+        else:
+            jsonObj=rendAllStocks()
+        context = {'jsonObj': jsonObj}
+        return render(request, 'index.html', context )
     else:
         if request.method == 'POST':
             username = request.POST.get('username')
@@ -45,7 +108,9 @@ def loginPage(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return render(request,'index.html')
+                jsonObj=rendAllStocks()
+                context = {'jsonObj': jsonObj}
+                return render(request,'index.html',context)
             else:
                 messages.info(request, 'Username OR password is incorrect')
         context = {}
@@ -56,55 +121,3 @@ def logoutUser(request):
 	logout(request)
 	return redirect('login')
 
-
-
-@login_required(login_url='login')
-def redisDBandCache(request):
-    filter_rec = request.GET.get('stock')
- 
-    if filter_rec is not None:
-        uperc=filter_rec.upper()
-        if cache.get(uperc):
-            # uperc=filter_rec.upper()
-            jsonObj = cache.get(uperc)
-            # print(newlist)
-            print("DATA COMING FROM CACHE")
-        else:
-            # uperc=filter_rec.upper()
-            req_str = f'bhavcopy:{uperc}*'
-            mylist= []
-            for key in r.scan_iter(req_str):
-                mylist.append(key.decode('utf-8'))
-            mylistdeteils = []
-            for l in mylist:
-                mylistdeteils.append(r.hgetall(l))
-            finallist=[]
-            for x in mylistdeteils:
-                finallist.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in x.items()})
-
-            newlist = sorted(finallist, key=lambda k: k['SC_CODE']) 
-            jsonObj= json.dumps(newlist,separators=(',', ':')).replace("'", r"\'")
-            cache.set(uperc, jsonObj,timeout=500)
-            print("DATA COMING FROM DB")
-    else:
-        print("All data load on startup")
-        myallkeys = []
-        for k in r.keys():
-            myallkeys.append(k)
-
-        myallvalues =[]
-        for s in myallkeys:
-            myallvalues.append(r.hgetall(s))
-
-        finalAllList =[]
-        for n in myallvalues:
-            finalAllList.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in n.items()})
-        newlist = sorted(finalAllList, key=lambda k: k['SC_CODE']) 
-        jsonObj= json.dumps(newlist,separators=(',', ':')).replace("'", r"\'")
-
-    context = {'jsonObj': jsonObj}
-    return render(request, 'index.html', context )
-
-
-
-        
