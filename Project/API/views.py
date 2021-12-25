bccdcd732d7ec8a0a599a7277434693a7037fb7b
ShubhamNagure <nagure.shubham@gmail.com>
2021-12-25 17:32:59 +0000

    stock search logic implemented

--- Project/API/views.py
+++ Project/API/views.py
@@ -3,6 +3,7 @@ from rest_framework import routers, serializers, viewsets
 
 from MainApp.models import BhavcopyRec
 from .serializers import BhavcopyRecSerializer
+from django.core.cache import cache
 
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -18,29 +19,51 @@ host='127.0.0.1',
 port='6379')
 
 
+def pullDataFromRedis():
+    myallkeys = []
+    for k in r.keys():
+        myallkeys.append(k)
+
+    myallvalues =[]
+    for s in myallkeys:
+        myallvalues.append(r.hgetall(s))
+
+    finalAllList =[]
+    for n in myallvalues:
+        finalAllList.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in n.items()})
+    newlist = sorted(finalAllList, key=lambda k: k['SC_CODE']) 
+    return newlist
+
 def getAllStocks(request):
-    print("All data load on startup")
-
     if request.method =='GET':
-    
-        myallkeys = []
-        for k in r.keys():
-            myallkeys.append(k)
-
-        myallvalues =[]
-        for s in myallkeys:
-            myallvalues.append(r.hgetall(s))
-
-        finalAllList =[]
-        for n in myallvalues:
-            finalAllList.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in n.items()})
-        newlist = sorted(finalAllList, key=lambda k: k['SC_CODE']) 
-        jsonObj= json.dumps(newlist,separators=(',', ':')).replace("'", r"\'")
-
-        
-        return JsonResponse(jsonObj, safe=False)
+        redis_list=pullDataFromRedis()
+        serialize= BhavcopyRecSerializer(data=redis_list,many=True)
+        return JsonResponse(serialize.initial_data,safe=False)
     
     
-def searchStocks(request):
-    pass
+def searchStocks(request,name):
+    filter_rec = name
+ 
+    uperc=filter_rec.upper()
+    if cache.get(uperc):
+        # uperc=filter_rec.upper()
+        jsonObj = cache.get(uperc)
+        # print(newlist)
+        print("DATA COMING FROM CACHE")
+    else:
+        # uperc=filter_rec.upper()
+        req_str = f'bhavcopy:{uperc}*'
+        mylist= []
+        for key in r.scan_iter(req_str):
+            mylist.append(key.decode('utf-8'))
+        mylistdeteils = []
+        for l in mylist:
+            mylistdeteils.append(r.hgetall(l))
+        finallist=[]
+        for x in mylistdeteils:
+            finallist.append({k.decode('utf-8').strip(): v.decode('utf-8').strip() for k ,v in x.items()})
+
+        newlist = sorted(finallist, key=lambda k: k['SC_CODE']) 
+        serialize= BhavcopyRecSerializer(data=newlist,many=True)
+        return JsonResponse(serialize.initial_data,safe=False)
 
